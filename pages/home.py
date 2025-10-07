b9551152d0db674b854f24d991694fcd133f1b00
Emil Robert Melconian <emelconi@uwo.ca>
2025-10-06 21:46:13 -0400

    some starts

---
+++ pages/home.py
@@ -0,0 +1,8 @@
+import streamlit as st
+# gonna have to import from other .py files
+
+# 
+st.set_page_config(page_title="Equities Portfolio Visualizer", layout="wide", page_icon="📊")
+
+st.sidebar.title("Home")
+(st.sidebar.button(..., use_container_width=True))
